a867d135efe458750a3ab668e3fd3d449d13297e
recep <recep@recep-300E4A-300E5A-300E7A>
2015-07-11 19:10:08 +0300

    first commit

---
+++ sharedTest/foo.c
@@ -0,0 +1,14 @@
+/*
+ * foo.c
+ *
+ *  Created on: Jul 1, 2015
+ *      Author: recep
+ */
+
+
+#include <stdio.h>
+
+void foo(void)
+{
+    puts("Hello, I'm a shared library");
+}
---
+++ sharedTest/foo.h
@@ -0,0 +1,14 @@
+/*
+ * foo.h
+ *
+ *  Created on: Jul 1, 2015
+ *      Author: recep
+ */
+
+#ifndef FOO_H_
+#define FOO_H_
+
+extern void foo(void);
+
+
+#endif /* FOO_H_ */
